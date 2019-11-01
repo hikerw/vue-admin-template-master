774c0b133e6944e9e18bedffcced155a145e80a0
hikerw <965366906@qq.com>
2019-11-01 16:09:02 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,9 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  // 基本路径
+ 
+  publicPath: './',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
